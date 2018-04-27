554fc1a6f00c2245130d50bf9ae94c204940c707
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-04-27 00:20:15 -0700

    added route for get one tag

--- src/controllers/tags.js
+++ src/controllers/tags.js
@@ -2,19 +2,23 @@ const model = require('../models/tags');
 
 function getAll (req, res, next) {
   model.getAll()
-  .then((data) => {
+  .then(data => {
     res.status(200).send({data});
   })
-  .catch(next)
+  .catch(next);
 }
 
 function getOne (req, res, next) {
-  const tag = model.getOne(req.params.id);
-  if (tag.data) {
-    res.status(200).send({ data: tag.data });
-  } else if (tag.error) {
-    return next({ status: 404, message: tag.error });
-  }
+  model.getOne(parseInt(req.params.id))
+  .then(data => {
+    if (data) {
+      return res.status(200).send({ data })
+    }
+    else {
+      throw { status: 404, message: 'Tag not found' }
+    }
+  })
+  .catch(next);
 }
 
 module.exports = { getAll, getOne };
--- src/models/tags.js
+++ src/models/tags.js
@@ -17,7 +17,44 @@ function getAll () {
 }
 
 function getOne (id) {
-  return db('tags').where({ id: id }).first();
+  return db('tags')
+  .where({ id: id})
+  .then(tags => {
+    const promises = tags.map(tag => {
+      return db('posts_tags')
+      .join('posts', 'posts.id', 'posts_tags.post_id')
+      .where('posts_tags.tag_id', tag.id)
+      .then(posts => {
+        const authorPromises = posts.map(post => {
+          return db('users')
+          .where('id', post.user_id)
+          .first()
+          .then(user => {
+            post.author = user;
+            return post;
+          });
+        });
+        return Promise.all(authorPromises);
+      })
+      .then(posts => {
+        const postPromises = posts.map(post => {
+          return db('posts_tags')
+          .join('tags', 'tags.id', 'posts_tags.tag_id')
+          .where('posts_tags.post_id', post.id)
+          .then(tags => {
+            post.tags = tags;
+            return post;
+          });
+        });
+        return Promise.all(postPromises);
+      })
+      .then(posts => {
+        tag.posts = posts;
+        return tag;
+      })
+    });
+    return Promise.all(promises);
+  });
 }
 
 module.exports = { getAll, getOne };
