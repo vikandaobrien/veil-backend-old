8e4146c272b6bb636bf43d6801d9a233a26e0d9b
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-04-27 05:00:42 -0700

    added routes for games

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -2,19 +2,23 @@ const model = require('../models/games');
 
 function getAll (req, res, next) {
   model.getAll()
-  .then((data) => {
+  .then(data => {
     res.status(200).send({data});
   })
-  .catch(next)
+  .catch(next);
 }
 
 function getOne (req, res, next) {
-  const game = model.getOne(req.params.id);
-  if (game.data) {
-    res.status(200).send({ data: game.data });
-  } else if (game.error) {
-    return next({ status: 404, message: game.error });
-  }
+  model.getOne(parseInt(req.params.id))
+  .then(data => {
+    if (data) {
+      return res.status(200).send({ data })
+    }
+    else {
+      throw { status: 404, message: 'Game not found' }
+    }
+  })
+  .catch(next);
 }
 
 module.exports = { getAll, getOne };
--- src/models/games.js
+++ src/models/games.js
@@ -5,7 +5,20 @@ function getAll () {
 }
 
 function getOne (id) {
-  return db('games').where({ id: id }).first();
+  return db('games')
+  .where({ id: id })
+  .then(games => {
+    const promises = games.map(game => {
+      return db('users_games')
+      .join('users', 'users.id', 'users_games.user_id')
+      .where('users_games.game_id', game.id)
+      .then(users => {
+        game.users = users;
+        return game;
+      });
+    });
+    return Promise.all(promises);
+  });
 }
 
 module.exports = { getAll, getOne };
